4e512dfcbd47b70e55e94dfa93a0a82641e4ac32
Zkang40 <87984093+zhaodong03@users.noreply.github.com>
2024-04-14 22:14:55 -0400

    add intro

--- home.py
+++ home.py
@@ -22,14 +22,14 @@ Are you ready to unlock the power of data analysis and visualization with Python
 Python is a high-level programming language that become one of the most popular programming languages due to its simplicity, readability, and versatility. Python is used in a wide range of fielding including web development, data analysis, AI, machine learning, and etc. This course chose Python as the programming language because it is beginner-friendly, versatile, has a strong community and resources, and is FREE. 
 
 ## What You'll Learn?
-Python Basics: Get comfortable with Python syntax and write your first code
-Data Structures: Learn how you could store data in Python and how to manipulate them
-Data Analysis Tools: Learn how to utilize various different Python libraries to analyze and visualize your data 
+1. Python Basics: Get comfortable with Python syntax and write your first code
+2. Data Structures: Learn how you could store data in Python and how to manipulate them
+3. Data Analysis Tools: Learn how to utilize various different Python libraries to analyze and visualize your data 
 
 ## Who Is This For?
-Complete Beginners: If you have zero programming experience but want to learn Python, this is the perfect place to start
-Non-Technical Professionals: Whether you're a marketer, business analyst, or researcher, learning Python through this course would supercharge your data skills and career prospects
-Students: If you're a student looking to add valuable programming skills to your toolkit, this course provides a solid foundation in Python programming for data work.
+1. Complete Beginners: If you have zero programming experience but want to learn Python, this is the perfect place to start
+2. Non-Technical Professionals: Whether you're a marketer, business analyst, or researcher, learning Python through this course would supercharge your data skills and career prospects
+3. Students: If you're a student looking to add valuable programming skills to your toolkit, this course provides a solid foundation in Python programming for data work.
 
 ## How to Use This Learning Platform
 Firstly, please follow the Python Installation instructions to ensure all required toolkits are correctly installed on your machine. Then you could click the first module to begin your programming journey
--- pages/2_python_introduction.py
+++ pages/2_python_introduction.py
@@ -3,24 +3,160 @@ import streamlit as st
 tab1, tab2, tab3 = st.tabs(["Objectives", "Course", "Assignment"])
 
 with tab1:
-   st.header("Module 1: Python Syntax")
-   st.markdown('''
-## Course Objectives
-               
-In this lesson, "Basic Syntax and Comments," you'll take your first steps into writing clean and readable code. Python is known for its straightforward syntax, often compared to the English language, which makes it an excellent language for beginners.
+    st.header("Module 1: Python Syntax")
+    st.markdown('''
+    ## Course Objectives
+                
+    In this lesson, "Basic Syntax and Comments," you'll take your first steps into writing clean and readable code. Python is known for its straightforward syntax, often compared to the English language, which makes it an excellent language for beginners.
 
-You will learn about the structure of a Python program, which serves as the backbone of your coding journey. We'll demystify how Python uses whitespace to organize code, a feature that makes Python unique. This concept, called indentation, is not just about making your code look pretty — it's essential for your program to run correctly.
+    You will learn about the structure of a Python program, which serves as the backbone of your coding journey. We'll demystify how Python uses whitespace to organize code, a feature that makes Python unique. This concept, called indentation, is not just about making your code look pretty — it's essential for your program to run correctly.
 
-In addition, you'll discover the art of commenting your code. Comments are the silent guides that talk to you and other programmers, explaining what the code does and why certain decisions were made. They are crucial for teamwork and maintaining code in the long run.
+    In addition, you'll discover the art of commenting your code. Comments are the silent guides that talk to you and other programmers, explaining what the code does and why certain decisions were made. They are crucial for teamwork and maintaining code in the long run.
 
-By the end of this lesson, you'll be able to craft a simple Python program and understand the importance of comments in making your code understandable. Let's get started and write some beautiful Python code!             
+    By the end of this lesson, you'll be able to craft a simple Python program and understand the importance of comments in making your code understandable. Let's get started and write some beautiful Python code!             
 
-## Course Sections
-Section 01:  Basic Syntax and Comments
-Section 02: Variables and Data Types             
-''')
+    ## Course Sections
+    Section 01:  Basic Syntax and Comments
+
+    Section 02: Variables and Data Types             
+    ''')
 with tab2:
-   st.header("Course")
+    st.header("Course")
+    st.markdown('''
+    ## Section 01: Basic Syntax and Comments
+    In this lesson, "Basic Syntax and Comments," you'll take your first steps into writing clean and readable code. Python is known for its straightforward syntax, often compared to the English language, which makes it an excellent language for beginners.
+
+    You will learn about the structure of a Python program, which serves as the backbone of your coding journey. We’ll demystify how Python uses whitespace to organize code, a feature that makes Python unique. This concept, called indentation, is not just about making your code look pretty — it’s essential for your program to run correctly.
+
+    In addition, you'll discover the art of commenting your code. Comments are the silent guides that talk to you and other programmers, explaining what the code does and why certain decisions were made. They are crucial for teamwork and maintaining code in the long run.
+
+    By the end of this lesson, you'll be able to craft a simple Python program and understand the importance of comments in making your code understandable. Let’s get started and write some beautiful Python code!
+
+    ## Table of Content
+    - Structure of a Python program
+    - Understanding indentation
+    - Writing comments
+    - Exercise -- Writing first Python Program
+    - Quiz
+
+
+    ## Structure of a Python Program        
+    - Python programs are composed of lines of code and follow a specific syntax.
+    - A simple Python script often starts with imports (if any), followed by definitions of functions and classes, and finally the main code body.
+    - Example of a basic Python program structure:
+    TODO: IMAGE
+        
+    ## Understanding Indentation
+    - Indentation refers to the spaces at the beginning of a code line.
+    - In Python, indentation is crucial as it defines the scope of loops, functions, classes, and conditionals (like if statements).
+    - Consistent indentation is key; Python does not allow mixing tabs and spaces.
+    - Example of correct and incorrect indentation:
+        - Correct:
+        TODO: IMAGE
+        - Incorrect:
+    TODO: IMAGE
+            
+    ## Writing Comment
+    - Comments are lines that are not executed and are used to explain the code.
+    - Single-line comments: Start with a hash character (#) and extend to the end of the line.
+        - Example:
+    TODO: IMAGE
+    - Multi-line comments: While Python does not have a direct way to create multi-line comments, you can use a multi-line string (triple quotes) that is not assigned to a variable.
+        - Example:
+    TODO: IMAGE
+
+    ## Quiz
+    ### Quiz 01: Section 01 -- Getting Started with Python Programming
+    ''')
+    q1 = st.radio(
+        "What is the correct way to start a single-line comment in Python?",
+        ["// This is a comment", "/* This is a comment */", "# This is a comment", "<!-- This is a comment -->"],
+        index=None,
+    )
+    q2 = st.radio(
+        "Which of the following statements about Python indentation is true?",
+        ["Indentation is optional in Python and does not affect the code execution.", "In Python, indentation is used for aesthetics only and has no practical use.","Indentation in Python is critical and is used to define the scope of loops, functions, and classes.","Python code can be indented using any number of spaces or tabs interchangeably without issue."],
+        index=None,
+    )
+    q3 = st.radio(
+        "How do you write a multi-line comment in Python?",
+        ["Surround the comment with /* and */.", "Start each line of the comment with #.", 'Enclose the comment in triple double-quotes, like """This is a multi-line comment""".', "Python does not support multi-line comments."],
+        index=None,
+    )
+
+    st.subheader("You must correctly answer the above three quizzes to unlock the rest masterials")
+    if q1 == "// This is a comment" and q2 == "Indentation in Python is critical and is used to define the scope of loops, functions, and classes." and q3 == "Surround the comment with /* and */.":
+        st.markdown('''
+    ## Section 02: Variables and Data Types
+    In this section, we will dive into how you can declare variables and the simplicity behind Python’s dynamic typing. We’ll explore the core data types that are foundational to programming in Python: integers for whole numbers, floating-point numbers for decimals, strings for text, and booleans for truth values.
+
+    Understanding these basics is like learning the alphabet before writing sentences. Once you grasp these concepts, you’ll be well on your way to creating more complex and powerful Python programs. Let's get started and discover the simplicity and power of Python's variables and data types!
+
+    ## Table of Content
+    - Declaring and Using Variable
+    - Common Data Types
+    - Dynamic Data Types
+    -Quiz
+    ## Declaring and Using Variables
+    In Python, variables are like containers for storing data values. Unlike other programming languages, there is no need to declare the type of variable; it is inferred from the value it is assigned.
+
+    To create a variable, you simply assign it a value using the equals sign (=).
+
+    Example:
+                    
+    Python has a set of built-in data types that define the type of value a variable can hold. Here are the most common ones:
+                    
+    Integers (int)
+                    
+    Whole numbers without a fractional part.
+                    
+    Example:
+    
+    > age = 21
+                    
+    Floating-Point Numbers (float)
+
+    Numbers with a decimal point or in exponential form.
+
+    Example:
+    > temperature = 98.6
+
+    Booleans (bool)
+    
+    Represents True or False values and is used to perform logical operations.
+
+    Example:  
+
+    >is_active = False     
+
+    ## Dynamic Typing in Python
+    - Python is dynamically typed, which means you don't have to explicitly declare the data type of a variable. The type can change as the assignment to the variable changes.
+    - This feature allows for greater flexibility, but it also means that you need to be aware of the type of value your variables are holding.
+    - Example of dynamic typing:
+         
+''')
+    rate = st.slider('Please Rate this module', 0, 3, 5)
 
 with tab3:
-   st.header("Assignment")
+    st.header("Assignment")
+    st.markdown('''         
+    ## Objective: 
+    Create a Python script that demonstrates the use of basic syntax, comments, variables, and data types. Your script will act as a journal entry for your first week of learning Python, summarizing what you have learned and storing various pieces of information as Python variables.
+    Assignment Details:
+                
+    1. Create a Header Comment:
+    At the beginning of your script, include a header comment with the title of the assignment, your name, and the date.
+    2. Declare Variables:
+    - Declare a string variable to store the title of the module.
+    - Declare an integer variable to count the number of lessons completed.
+    - Declare a float variable to estimate how much of the module you understand as a percentage.
+    - Declare a boolean variable to state whether you have completed the assignment or not.
+    3. Add Descriptive Comments:
+    For each variable declaration, add a line comment that explains what the variable represents.
+    4. Print Variables:
+    Use the print function to output each variable on a separate line.
+    Include a string literal before printing each variable to describe what is being printed.''')
+    file = st.file_uploader('',['py'])
+
+    st.markdown("## Grade & Feedback")
+   
